6940ca4f2ae6de2e26db48fb57a4ab6f77a9a752
Alexey Borisenko <bgr666@gmail.com>
2024-01-26 15:15:43 +0300

    hw14_0.3

--- src/features/blogs/api/dto/input/blogs-input-dto.ts
+++ src/features/blogs/api/dto/input/blogs-input-dto.ts
@@ -5,16 +5,16 @@
 //         public websiteUrl: string){}
 // }
 
-import { Length } from 'class-validator';
+import { Length, Matches } from 'class-validator';
 
 export class BlogPostType {
-  @Length(3, 10)
+  @Length(1, 15)
   name: string;
 
-  @Length(3, 10)
+  @Length(1, 500)
   description: string;
 
-  @Length(3, 30)
+  @Matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
   websiteUrl: string;
 }
 
