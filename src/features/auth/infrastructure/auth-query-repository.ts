d915d16ffc5f9e6256226353c593a85d032869d6
Alexey Borisenko <bgr666@gmail.com>
2024-02-20 19:22:33 +0300

    hw16_2.2

--- src/features/auth/infrastructure/auth-query-repository.ts
+++ src/features/auth/infrastructure/auth-query-repository.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { AuthDocument } from '../domain/auth-entity';
+import { Auth, AuthDocument } from '../domain/auth-entity';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import { User } from '../../users/domain/users-entity';
 import { AuthTypeOutput } from '../api/dto/output/auth-output-dto';
 
 @Injectable()
 export class AuthQueryRepository {
-  constructor(@InjectModel(User.name) private AuthModel: Model<AuthDocument>) {}
+  constructor(@InjectModel(Auth.name) private AuthModel: Model<AuthDocument>) {}
 
   async findAuthSessionsByUserId(
     userId: string,
