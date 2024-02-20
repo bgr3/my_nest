89b0f8d4797d1b9ac92ac43aa137fa57f1ab28fe
Alexey Borisenko <bgr666@gmail.com>
2024-02-20 19:59:22 +0300

    hw16_2.4

---
+++ src/infrastructure/middlewares/security-validation-middleware.ts
@@ -0,0 +1,27 @@
+import { HttpException, Injectable, NestMiddleware } from '@nestjs/common'
+import { Request, Response, NextFunction } from 'express'
+import { HTTP_STATUSES } from '../../settings/http-statuses'
+import { AuthRepository } from '../../features/auth/infrastructure/auth-repository';
+
+@Injectable()
+export class AuthorizationSecurityMiddleware implements NestMiddleware{
+    constructor(
+        protected authQueryRepository: AuthRepository,
+        ){}
+    async use(req: Request, res: Response, next: NextFunction) {
+        const DeviceId = req.params[0];
+        const auth = await this.authQueryRepository.findAuthSessionByDeviceId(DeviceId)
+        const userId = req.user
+        
+        if (auth && userId) {
+            if (auth.userId === userId) {
+                next()
+                return
+            } else {
+                throw new HttpException('', HTTP_STATUSES.FORBIDDEN_403);
+            }
+        }
+
+        next() 
+    }
+}
